808d2d92a4e80ee0498dac4c02ed3cb7a88b6ba7
dslsyeoh <syeoh91x@gmail.com>
2019-09-21 01:52:10 +0800

    add Operator enum

---
+++ src/com/dsl/simple/script/engine/constants/Operator.java
@@ -0,0 +1,16 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.simple.script.engine.constants;
+
+public enum Operator
+{
+    EQUAL,
+    NOT_EQUAL,
+    LESS_THAN,
+    NOT_MORE_THAN,
+    MORE_THAN,
+    NOT_LESS_THAN;
+}
