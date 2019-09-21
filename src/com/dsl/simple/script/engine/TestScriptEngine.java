fda0fbf2c802d4707dd9d8ffa0fb427e184a00a2
dslsyeoh <syeoh91x@gmail.com>
2019-09-21 17:30:21 +0800

    update

--- src/com/dsl/simple/script/engine/TestScriptEngine.java
+++ src/com/dsl/simple/script/engine/TestScriptEngine.java
@@ -7,7 +7,6 @@ package com.dsl.simple.script.engine;
 
 import com.dsl.simple.script.engine.data.DataStorage;
 import com.dsl.simple.script.engine.lib.ScriptEngine;
-import com.dsl.simple.script.engine.lib.ScriptEngineV2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,21 +18,17 @@ public class TestScriptEngine
         DataStorage dataStorage = new DataStorage();
         dataStorage.mockData().forEach(dataStorage::update);
 
-//        ScriptEngine.init(dataStorage.getData());
-//        List<String> scripts = Arrays.asList("${PARAM_1} + ${PARAM_2} == ${PARAM_3}", "${PARAM_4} == 20", "${PARAM_3} == 10", "${PARAM_5} == ", "${PARAM_1} + ${PARAM_2} + ${PARAM_3} == 20",
-//                "${PARAM_1} - ${PARAM_2} == 0", "${PARAM_1} * ${PARAM_2} == 25", "${PARAM_1} * ${PARAM_2} + ${PARAM_4} == 45", "${PARAM_1} / ${PARAM_2} == 1",
-//                "${PARAM_1} * ${PARAM_2} + ${PARAM_3} + ${PARAM_4} / ${PARAM_1} == 11");
-//        System.out.println(scripts.stream().allMatch(ScriptEngine::eval));
-//
-//        scripts = Arrays.asList("${PARAM_1} + ${PARAM_2} == ${PARAM_3}", "${PARAM_4} == 20");
-//        System.out.println(scripts.stream().allMatch(ScriptEngine::eval));
+        ScriptEngine.init(dataStorage.getData());
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} == ${PARAM_3}"));
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} <= ${PARAM_3}"));
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} < ${PARAM_3}"));
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} >= ${PARAM_3}"));
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} > ${PARAM_3}"));
+        System.out.println(ScriptEngine.eval("${PARAM_1} + ${PARAM_2} != ${PARAM_4}"));
 
-        ScriptEngineV2.init(dataStorage.getData());
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} == ${PARAM_3}"));
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} <= ${PARAM_3}"));
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} < ${PARAM_3}"));
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} >= ${PARAM_3}"));
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} > ${PARAM_3}"));
-        System.out.println(ScriptEngineV2.eval("${PARAM_1} + ${PARAM_2} != ${PARAM_4}"));
+        List<String> scripts = Arrays.asList("${PARAM_1} + ${PARAM_2} == ${PARAM_3}", "${PARAM_4} == 20", "${PARAM_3} == 10", "${PARAM_5} == ", "${PARAM_1} + ${PARAM_2} + ${PARAM_3} == 20",
+                "${PARAM_1} - ${PARAM_2} == 0", "${PARAM_1} * ${PARAM_2} == 25", "${PARAM_1} * ${PARAM_2} + ${PARAM_4} == 45", "${PARAM_1} / ${PARAM_2} == 1",
+                "${PARAM_1} * ${PARAM_2} + ${PARAM_3} + ${PARAM_4} / ${PARAM_1} == 11");
+        System.out.println(scripts.stream().allMatch(ScriptEngine::eval));
     }
 }
